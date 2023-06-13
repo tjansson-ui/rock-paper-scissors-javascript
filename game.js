56b8671ac3cde6aeffd4359192c8a92af5890516
tjansson-ui <59398067+tjansson-ui@users.noreply.github.com>
2023-06-12 20:58:18 -0500

    added choice and psuedocode

--- game.js
+++ game.js
@@ -1,1 +1,35 @@
-console.log("Hello RPS!")
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+// Pseudo-code
+
+//human player
+var choices = ["R","P","S"] 
+
+//computer player
+var choice = {
+    human: prompt("Type R, P, or S"),
+    computer: function randomNum() {
+        n = Math.floor(Math.random() * 3);
+        choices[n];
+    }
+}
+// var result = [win]
+
+
+//human choose R,P,S
+
+    //get choice from user input
+    //computer chooses R,P,S randomly
+
+//compare choices
+    //if humanChoice is S and computerChoice is P OR
+// var result = []
+
+// function() 
+//     human.result[0] ? "Congrats, you win! Play Again?"
+//     :human.result[1] ? "Oh... Looks like you lost"
+//     :human.result[2] ? "It's a tie. Let's play again"
+
+//var hunger = hungerlevel >= 50 ? 'I am really hungry' : "I'm full for now..."
