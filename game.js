fce23b2bb55b0867f39fc69c11bc5d64a59fcd59
tjansson-ui <59398067+tjansson-ui@users.noreply.github.com>
2023-06-12 21:32:00 -0500

    completed the window side of game

--- game.js
+++ game.js
@@ -8,28 +8,34 @@ var ties = 0;
 var choices = ["R","P","S"] 
 
 //computer player
-var choice = {
-    human: prompt("Type R, P, or S"),
-    computer: choices[ Math.floor(Math.random() * 3)],
+var playAgain = true
+
+while (playAgain) {
+    var choice = {
+        human: prompt("Type R, P, or S"),
+        computer: choices[ Math.floor(Math.random() * 3)],
+        }
+    
+    console.log("Human: " + choice.human)
+    console.log("Computer: " + choice.computer)
+    
+    if (choice.human === "S" && choice.computer === "P" ||
+        choice.human === "R" && choice.computer === "S" ||
+        choice.human === "P" && choice.computer === "R" )
+        {
+            alert("You Won!")
+            wins++
     }
-
-console.log("Human: " + choice.human)
-console.log("Computer: " + choice.computer)
-
-if (choice.human === "S" && choice.computer === "P" ||
-    choice.human === "R" && choice.computer === "S" ||
-    choice.human === "P" && choice.computer === "R" )
-    {
-        alert("You Won!")
-        wins++
-}
-
-else if (choice.human === choice.computer) {
-    alert("Round Tied!")
-    ties++
-} else {
-    alert("You lost!")
-    losses++
+    
+    else if (choice.human === choice.computer) {
+        alert("Round Tied!")
+        ties++
+    } else {
+        alert("You lost!")
+        losses++
+    }
+    alert("Wins: " + wins + "\nTies: " + ties + "\nLosses: " + losses)
+    playAgain = confirm("Want to play again?")
 }
 // var result = [win]
 
@@ -46,9 +52,8 @@ else if (choice.human === choice.computer) {
 //     choice.human === "R" && choice.computer === "S" ||
 //     choice.human === "P" && choice.computer === "R"  ? "Congrats, you win! Play Again?" wins++
 //     :choice.human === choice.computer ? "It's a tie. Let's play again" ties++
-//     : "You Lost..." losses++;
+//     : "You Lost..." losses++; alert(result)
 
-// alert(result)
-alert("Wins: " + wins + "\nTies: " + ties + "\nLosses: " + losses)
+
 //score
 
